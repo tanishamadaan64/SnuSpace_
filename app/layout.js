6a89e2f26425160af3714fe8e0f616f8db6724c8
tan <117458118+tanishamadaan64@users.noreply.github.com>
2025-06-12 11:43:53 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,15 @@ export default function RootLayout({ children }) {
           strategy="lazyOnload"
         />
       </head>
-      
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
